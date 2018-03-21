b23d7512be3400f5241668462867a9bf96a592ff
Eduardo Tongson <propolice@gmail.com>
2018-03-21 11:24:46 +0800

    development: No longer need to copy luacheck and luacov files since we have a proper LUA_PATH now.

--- lib/rules.mk
+++ lib/rules.mk
@@ -35,14 +35,6 @@ $(VENDOR_LUA):
 	$(ECHOT) CP VENDOR_DIR
 	for d in $(VENDOR_DIRS); do [ -d $$d ] || $(CPR) $(VENDOR_P)/$$d .; done
 
-luacheck:
-	$(ECHOT) CP luacheck
-	$(CPR) lib/luacheck .
-
-luacov:
-	$(ECHOT) CP luacov
-	$(CPR) lib/luacov .
-
 $(EXE_T): $(BUILD_DEPS) $(LIBLUAJIT_A) $(LUA_T) $(COMPILED_FNL) $(VENDOR_TOP) $(SRC_TOP) $(SRC_LUA) $(VENDOR_LUA)
 	$(ECHOT) LN $(EXE_T)
 	CC=$(TARGET_STCC) NM=$(TARGET_NM) $(LUA_T) $(LUASTATIC) $(MAIN) \
@@ -51,7 +43,7 @@ $(EXE_T): $(BUILD_DEPS) $(LIBLUAJIT_A) $(LUA_T) $(COMPILED_FNL) $(VENDOR_TOP) $(
 	$(RM) $(RMFLAGS) $(MAIN).c $(VENDOR_TOP) $(SRC_TOP)
 	$(RMRF) $(VENDOR_DIRS) $(SRC_DIRS)
 
-development: $(LUA_T) luacheck luacov $(COMPILED_FNL) $(VENDOR_LUA) $(VENDOR_TOP)
+development: $(LUA_T) $(COMPILED_FNL) $(VENDOR_LUA) $(VENDOR_TOP)
 	for f in $(SRC); do $(CP) $(SRC_P)/$$f.lua .; done
 	$(RMRF) $(SRC_DIRS)
 	for d in $(SRC_DIRS); do $(CPR) $(SRC_P)/$$d .; done
