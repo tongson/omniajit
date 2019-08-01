543e81677644942d176f12f00a6ab1b895219543
Eduardo Tongson <propolice@gmail.com>
2019-08-01 14:33:45 +0800

    buildah: Better as debug messages.

--- vendor/lua/buildah.lua
+++ vendor/lua/buildah.lua
@@ -39,41 +39,41 @@ local from = function(base, cwd, name)
     local fn = {}
     fn.run = function(...)
         local a = pargs(...)
-        msg.info(sf("RUN %s", concat(a, " ")))
+        msg.debug(sf("RUN %s", concat(a, " ")))
         exe("run", name, "--", unpack(a))
     end
     fn.apt_get = function(...)
         local a = pargs(...)
-        msg.info(sf("RUN apt-get %s", concat(a, " ")))
+        msg.debug(sf("RUN apt-get %s", concat(a, " ")))
         exe("run", name, "--", "/usr/bin/env", "LC_ALL=C", "DEBIAN_FRONTEND=noninteractive", "apt-get", "-qq",
         "--no-install-recommends", "-o APT::Install-Suggests=0", "-o APT::Get::AutomaticRemove=1", "-o Dpkg::Use-Pty=0",
         "-o Dpkg::Options::='--force-confdef'", "-o Dpkg::Options::='--force-confold'", unpack(a))
     end
     fn.copy = function(src, dest)
         dest = dest or '/'
-        msg.info(sf("COPY '%s' to '%s'", src, dest))
+        msg.debug(sf("COPY '%s' to '%s'", src, dest))
         exe("copy", name, src, dest)
     end
     fn.clear = function(d)
-        msg.info(sf("CLEAR %s", d))
+        msg.debug(sf("CLEAR %s", d))
         exe("run", name, "--", "/usr/bin/find", d, "-mindepth", "1", "-ignore_readdir_race", "-delete")
     end
     fn.mkdir = function(d)
-        msg.info(sf("MKDIR %s", d))
+        msg.debug(sf("MKDIR %s", d))
         exe("run", name, "--", "mkdir", "-p", d)
     end
     fn.rm = function(f)
-        msg.info(sf("RM %s", f))
+        msg.debug(sf("RM %s", f))
         exe("run", name, "--", "rm", "-r", f)
     end
     fn.entrypoint = function(s)
-        msg.info(sf("ENTRYPOINT %s", s))
+        msg.debug(sf("ENTRYPOINT %s", s))
         exe("config", "--entrypoint", s, name)
         exe("config", "--cmd", "''", name)
         exe("config", "--stop-signal", "TERM", name)
     end
     fn.push = function(cname, tag)
-        msg.info(sf("PUSH %s:%s", cname, tag))
+        msg.debug(sf("PUSH %s:%s", cname, tag))
         local tmpname = string.format("%s.%s", cname, util.random_string(16))
         exe("commit", "--format", "docker", "--squash", "--rm", name, "dir:"..tmpname)
         local awscli = exec.ctx("/usr/bin/aws")
