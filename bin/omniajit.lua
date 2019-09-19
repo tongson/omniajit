462921b75567f662b9737b31bf16490ecf937cdf
Eduardo Tongson <propolice@gmail.com>
2019-09-19 23:59:00 +0800

    omniajit: New and improved script interpreter.

--- bin/omniajit.lua
+++ bin/omniajit.lua
@@ -1,35 +1,36 @@
 local script = arg[1]
-local lib = require "cimicida"
-local ENV = lib
+local lib = require "lib"
+local ENV = {
+         lib = lib,
+    argparse = require "argparse",
+     inspect = require "inspect",
+         lfs = require "lfs",
+}
+local test = lib.file.test
+local string = string
 setmetatable(ENV, {__index = _G})
-local string, fmt, file, path = lib.string, lib.fmt, lib.file, lib.path
-package.path = path.split(script)
-local code = file.read_all(script)
-if not code then
-  return fmt.panic("error: problem reading script '%s'.\n", script )
+local fmt, util = lib.fmt, lib.util
+local reterr = function(tbl, err)
+    local ln = string.match(err, "^.+:([%d]):.*")
+    local sp = string.rep(" ", string.len(ln))
+    err = string.match(err, "^.+:[%d]:(.*)")
+    return fmt.panic("error: %s\n %s |\n %s | %s\n %s |\n", err, sp, ln, tbl[tonumber(ln)], sp)
 end
-local source
-if string.find(script, "fnl", "-3", true) then
-  local fennel = require "fennel"
-  local ok
-  ok, source = pcall(fennel.compileString, code)
-  if not ok then
-    return fmt.panic("error: problem transpiling Fennel script.\n")
-  end
-else
-  source = code
+package.path = util.split(script)
+if not test(script) then
+    return fmt.panic("error: problem reading script '%s'.\n", script )
 end
-local chunk, err = loadstring(source)
-if chunk then
-  setfenv(chunk, ENV)
-  return chunk()
-else
-  local tbl = {}
-  for ln in string.gmatch(source, "([^\n]*)\n*") do
+local tbl = {}
+for ln in io.lines(script) do
     tbl[#tbl + 1] = ln
-  end
-  local ln = string.match(err, "^.+:([%d]):.*")
-  local sp = string.rep(" ", string.len(ln))
-  err = string.match(err, "^.+:[%d]:(.*)")
-  return fmt.panic("error: %s\n%s |\n%s | %s\n%s |\n", err, sp, ln, tbl[tonumber(ln)], sp)
+end
+local chunk, err = loadstring(table.concat(tbl, "\n"), script)
+if chunk then
+    setfenv(chunk, ENV)
+    local pr, pe = pcall(chunk)
+    if not pr then
+        return reterr(tbl, pe)
+    end
+else
+    return reterr(tbl, err)
 end
