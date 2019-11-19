250a0f37237d6a0d5fb9912a57ee47810b2b6aa5
Eduardo Tongson <propolice@gmail.com>
2019-11-19 11:10:35 +0800

    moon: Bundle funk module.

--- bin/moon.mk
+++ bin/moon.mk
@@ -2,7 +2,7 @@
 EXE:= moon
 SRC:= 
 SRC_DIR:=
-VENDOR:= lib ring cliargs argparse lfs tsv dkjson ftcsv uuid CBOR salt lpcap lpcode lpeg lpprint lpvm
+VENDOR:= lib ring cliargs argparse lfs tsv dkjson ftcsv uuid CBOR salt funk lpcap lpcode lpeg lpprint lpvm
 VENDOR_DIR:= cliargs cliargs/utils moonscript moonscript/parse moonscript/compile moonscript/transform moonscript/cmd
 MAKEFLAGS= --silent
 HOST_CC= cc
