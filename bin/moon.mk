8e89e939a4e0c6aa77a31a826df0453dc3d7937c
Eduardo Tongson <propolice@gmail.com>
2020-04-14 17:36:54 +0800

    build: Remove cliargs, enable gc-sections flags and rearrange for clarity.

--- bin/moon.mk
+++ bin/moon.mk
@@ -2,13 +2,15 @@
 EXE:= moon
 SRC:= 
 SRC_DIR:=
-VENDOR:= lib ring cliargs argparse lfs tsv dkjson ftcsv uuid CBOR salt funk u-test base64 inspect templet xsys sqlite3 lpcap lpcode lpeg lpprint lpvm
-VENDOR_DIR:= cliargs cliargs/utils moonscript moonscript/parse moonscript/compile moonscript/transform moonscript/cmd
+VENDOR:= lib ring
+VENDOR+= dkjson uuid salt base64 inspect
+VENDOR+= argparse lfs lpcap lpcode lpeg lpprint lpvm
+VENDOR_DIR:= moonscript moonscript/parse moonscript/compile moonscript/transform moonscript/cmd
 MAKEFLAGS= --silent
 CC= cc
-CFLAGS= -Os -mtune=generic -mmmx -msse -msse2 -fomit-frame-pointer -pipe
+CFLAGS= -Os -mtune=generic -mmmx -msse -msse2 -fomit-frame-pointer -pipe -ffunction-sections -fdata-sections
 CCOPT=
-LDFLAGS= -Wl,--strip-all
+LDFLAGS= -Wl,--strip-all -Wl,--gc-sections
 include lib/tests.mk
 include lib/std.mk
 include lib/rules.mk
