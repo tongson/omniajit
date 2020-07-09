4ceb15c22e833a147fd442310bbfa1534cdc7bda
Eduardo Tongson <propolice@gmail.com>
2020-07-09 20:40:37 +0800

    moon: Match libluajit.a flags.

--- bin/moon.mk
+++ bin/moon.mk
@@ -8,7 +8,7 @@ VENDOR+= argparse lfs lpcap lpcode lpeg lpprint lpvm
 VENDOR_DIR:= moonscript moonscript/parse moonscript/compile moonscript/transform moonscript/cmd
 MAKEFLAGS= --silent
 CC= cc
-CFLAGS= -O2 -mtune=nocona -msse -msse2 -fomit-frame-pointer -pipe -ffunction-sections -fdata-sections
+CFLAGS= -O2 -mtune=generic -mmmx -msse -msse2 -fomit-frame-pointer -pipe -ffunction-sections -fdata-sections
 CCOPT=
 LDFLAGS= -Wl,--strip-all -Wl,--gc-sections
 include lib/tests.mk
