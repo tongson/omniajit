f5aff68a656b88c898a8245f25857623dc97ef1a
Eduardo Tongson <propolice@gmail.com>
2020-11-05 23:28:06 +0800

    build: Additional modules and no need for DNDEBUG.

--- bin/moon.mk
+++ bin/moon.mk
@@ -3,12 +3,12 @@ EXE:= moon
 SRC:= 
 SRC_DIR:=
 VENDOR:= lib ring
-VENDOR+= dkjson uuid salt base64 inspect lhutil
+VENDOR+= dkjson uuid salt base64 inspect lhutil lautil u-test sqlite3
 VENDOR+= argparse lfs lpcap lpcode lpeg lpprint lpvm
-VENDOR_DIR:= moonscript moonscript/parse moonscript/compile moonscript/transform moonscript/cmd lpeg_patterns lpeg_patterns/http
+VENDOR_DIR:= moonscript moonscript/parse moonscript/compile moonscript/transform moonscript/cmd lpeg_patterns lpeg_patterns/http cgilua
 MAKEFLAGS= --silent
 CC= cc
-CFLAGS= -O2 -march=nocona -mtune=haswell -msse4.2 -fomit-frame-pointer -pipe -ffunction-sections -fdata-sections -DNDEBUG
+CFLAGS= -O2 -march=nocona -mtune=haswell -msse4.2 -fomit-frame-pointer -pipe -ffunction-sections -fdata-sections
 CCOPT=
 LDFLAGS= -Wl,--strip-all -Wl,--gc-sections
 include lib/tests.mk
