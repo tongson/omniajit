c7e7be05c1748cdca39ebbf599003fe5fb68905a
Eduardo Tongson <propolice@gmail.com>
2020-11-06 18:57:17 +0800

    build: New modules to build.

--- bin/moon.mk
+++ bin/moon.mk
@@ -3,9 +3,9 @@ EXE:= moon
 SRC:= 
 SRC_DIR:=
 VENDOR:= lib ring
-VENDOR+= dkjson uuid salt base64 inspect lhutil lautil u-test sqlite3
+VENDOR+= uuid salt base64 inspect lhutil lautil u-test sqlite3 ftcsv lunajson
 VENDOR+= argparse lfs lpcap lpcode lpeg lpprint lpvm
-VENDOR_DIR:= moonscript moonscript/parse moonscript/compile moonscript/transform moonscript/cmd lpeg_patterns lpeg_patterns/http cgilua
+VENDOR_DIR:= moonscript moonscript/parse moonscript/compile moonscript/transform moonscript/cmd lpeg_patterns lpeg_patterns/http cgilua lunajson
 MAKEFLAGS= --silent
 CC= cc
 CFLAGS= -O2 -march=nocona -mtune=haswell -msse4.2 -fomit-frame-pointer -pipe -ffunction-sections -fdata-sections
