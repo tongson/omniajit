7f43c9e315626a6d7b765e258e4b83d35b0a5e79
Eduardo Tongson <propolice@gmail.com>
2020-11-09 19:48:55 +0800

    build: LTO build.

--- bin/moon.mk
+++ bin/moon.mk
@@ -1,16 +1,19 @@
 .DEFAULT_GOAL= release
 EXE:= moon
-SRC:= 
+SRC:=
 SRC_DIR:=
 VENDOR:= lib ring
 VENDOR+= uuid salt base64 inspect lhutil lautil u-test sqlite3 ftcsv lunajson curler etlua
 VENDOR+= argparse lfs lpcap lpcode lpeg lpprint lpvm
 VENDOR_DIR:= moonscript moonscript/parse moonscript/compile moonscript/transform moonscript/cmd lpeg_patterns lpeg_patterns/http cgilua lunajson
-MAKEFLAGS= --silent
-CC= cc
-CFLAGS= -O3 -march=nocona -mtune=haswell -msse4.2 -fomit-frame-pointer -pipe -ffunction-sections -fdata-sections
-CCOPT=
-LDFLAGS= -Wl,--strip-all -Wl,--gc-sections
+#MAKEFLAGS= --silent
+CC= gcc
+AR= gcc-ar
+NM= gcc-nm
+RANLIB= gcc-ranlib
+CFLAGS= -pipe -ffunction-sections -fdata-sections -fomit-frame-pointer -flto -fuse-linker-plugin
+CCOPT= -O3 -march=nehalem -mtune=haswell -msse4.2
+LDFLAGS= -Wl,--gc-sections,--as-needed,--sort-common,--strip-all,-flto
 include lib/tests.mk
 include lib/std.mk
 include lib/rules.mk
