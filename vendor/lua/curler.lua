fc9bee3c36d6770c3599f638e39f91786ec3c60d
Eduardo Tongson <propolice@gmail.com>
2020-11-27 19:03:56 +0800

    curler: Make it work with ffi exec.

--- vendor/lua/curler.lua
+++ vendor/lua/curler.lua
@@ -1,158 +1,201 @@
-local lib = require"lib"
-local util = lib.util
-local exec = lib.exec
-local json = require"lunajson"
+local clone = require 'table.clone'
+local exec = require 'exec'
+local json = require 'json'
+local ffi_ext = require 'ffi_ext'
+local random_string = ffi_ext.random_string
 local F = string.format
-local popen = exec.ctx()
-popen.ignore = true
 
-
-local user_agent = [[-A 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:70.0) Gecko/20100101 Firefox/70.0']]
-local defhdrs = [[-H 'Transfer-Encoding: chunked' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache']]
-local defopts = [[--retry 20 -m 5 --retry-delay 1 -s --tcp-fastopen --tcp-nodelay --http1.1 --location --referer ';auto']]
+local args = {
+  '-A',
+  [['Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:70.0) Gecko/20100101 Firefox/70.0']],
+  '-H', [['Transfer-Encoding: chunked']],
+  '-H', [['Connection: keep-alive']],
+  '-H', [['Pragma: no-cache']],
+  '-H', [['Cache-Control: no-cache']],
+  '--retry', '20',
+  '-m', '5',
+  '--retry-delay', '1',
+  '-s',
+  '--tcp-fastopen',
+  '--tcp-nodelay',
+  '--http1.1',
+  '--location',
+  '--referer', [[';auto']],
+}
 
 local json = function()
-  local headers = [[-H 'Content-Type: application/json' -H 'Accept: application/json']]
+  local a = clone(args)
+  a[#a+1] = '-H'
+  a[#a+1] = [['Content-Type: application/json']]
+  a[#a+1] = '-H'
+  a[#a+1] = [['Accept: application/json']]
   local set = {}
   set.headers = {}
   set.insecure = false
   set.save = false
   return setmetatable(set, {__call = function(_, url)
-    local curl_cookie = set.cookie or "/tmp/"..util.random_string(16)
-    local curl_insecure, curl_jar, curl_data
+    local cookie = set.cookie or '/tmp/'..random_string(16)
     if next(set.headers) then
       for h, v in pairs(set.headers) do
-        headers = headers .. (F(" -H '%s: %s'", h, v))
+        a[#a+1] = '-H'
+        a[#a+1] = F([['%s: %s']], h, v)
       end
     end
     if set.save then
-      curl_jar = F("-b %s -c %s", curl_cookie, curl_cookie)
+      a[#a+1] = '-b'
+      a[#a+1] = cookie
+      a[#a+1] = '-c'
+      a[#a+1] = cookie
     else
-      curl_jar = F("-b %s", curl_cookie)
+      a[#a+1] = '-b'
+      a[#a+1] = cookie
     end
     if set.insecure then
-      curl_insecure = "-k"
+      a[#a+1] = '-k'
     end
     if set.data then
-      curl_data = F("-d '%s'", set.data)
+      a[#a+1] = '-d'
+      a[#a+1] = F([['%s']], set.data)
       -- clear for next run
       set.data = nil
-      set.curl = F("curl %s %s %s %s %s %s %s %s", curl_data, curl_jar, headers, user_agent, defopts, defhdrs, curl_insecure or "", url)
-    else
-      set.curl = F("curl %s %s %s %s %s %s %s", curl_jar, headers, user_agent, defopts, defhdrs, curl_insecure or "", url)
     end
-    local n, r = popen(set.curl)
-    if n == 0 then
-      local ret, _, err = json.decode(r.output[1])
-      if err then
-        return nil, err
+    a[#a+1] = url
+    local n, r = exec.spawn('/usr/bin/curl', a)
+    if n then
+      local ret, s = pcall(json.decode, r.stdout[1])
+      if not ret then
+        return nil, s
       else
-        return ret
+        return s
       end
     else
-      return nil, F("error: popen: %s: %s", n, table.concat(r.output, "\n"))
+      return nil, F("error: exec:: %s: %s", n, r.error)
     end
   end})
 end
 
 local encoded = function()
-  local headers = [[-H 'Content-Type: application/x-www-form-urlencode' -H 'Accept: */*']]
+  local a = clone(args)
+  a[#a+1] = '-H'
+  a[#a+1] = [['Content-Type: application/x-www-form-urlencoded']]
+  a[#a+1] = '-H'
+  a[#a+1] = [['Accept: */*']]
   local set = {}
   set.headers = {}
   set.insecure = false
   set.save = false
   return setmetatable(set, {__call = function(_, url)
-    local curl_cookie = set.cookie or "/tmp/"..util.random_string(16)
-    local curl_insecure, curl_jar, curl_data
+    local cookie = set.cookie or '/tmp/'..random_string(16)
     if next(set.headers) then
       for h, v in pairs(set.headers) do
-        headers = headers .. (F(" -H '%s: %s'", h, v))
+        a[#a+1] = '-H'
+        a[#a+1] = F([['%s: %s']], h, v)
       end
     end
     if set.save then
-      curl_jar = F("-b %s -c %s", curl_cookie, curl_cookie)
+      a[#a+1] = '-b'
+      a[#a+1] = cookie
+      a[#a+1] = '-c'
+      a[#a+1] = cookie
     else
-      curl_jar = F("-b %s", curl_cookie)
+      a[#a+1] = '-b'
+      a[#a+1] = cookie
     end
     if set.insecure then
-      curl_insecure = "-k"
+      a[#a+1] = '-k'
     end
     if set.data then
-      curl_data = F("--data-urlencode '%s'", set.data)
+      a[#a+1] = '--data-urlencode'
+      a[#a+1] = F([['%s']], set.data)
       -- clear for next run
       set.data = nil
-      set.curl = F("curl %s %s %s %s %s %s %s %s", curl_data, curl_jar, headers, user_agent, defopts, defhdrs, curl_insecure or "", url)
-    else
-      set.curl = F("curl %s %s %s %s %s %s %s", curl_jar, headers, user_agent, defopts, defhdrs, curl_insecure or "", url)
     end
-    local n, r = popen(set.curl)
-    if n == 0 then
-      return table.concat(r.output, "\n")
+    a[#a+1] = url
+    local n, r = exec.spawn('/usr/bin/curl', a)
+    if n then
+      return table.concat(r.stdout, "\n")
     else
-      return nil, F("error: popen: %s: %s", n, table.concat(r.output, "\n"))
+      return nil, F("error: exec:: %s: %s", n, table.concat(r.stderr, "\n"))
     end
   end})
 end
 local get = function()
-  local headers = [[-H 'Accept: */*']]
+  local a = clone(args)
+  a[#a+1] = '-H'
+  a[#a+1] = [['Accept: */*']]
   local set = {}
   set.headers = {}
   set.insecure = false
   set.save = false
   return setmetatable(set, {__call = function(_, url)
-    local curl_cookie = set.cookie or "/tmp/"..util.random_string(16)
-    local curl_insecure, curl_jar, curl_data
+    local cookie = set.cookie or "/tmp/"..random_string(16)
     if next(set.headers) then
       for h, v in pairs(set.headers) do
-        headers = headers .. (F(" -H '%s: %s'", h, v))
+        a[#a+1] = '-H'
+        a[#a+1] = F([['%s: %s']], h, v)
       end
     end
     if set.save then
-      curl_jar = F("-b %s -c %s", curl_cookie, curl_cookie)
+      a[#a+1] = '-b'
+      a[#a+1] = cookie
+      a[#a+1] = '-c'
+      a[#a+1] = cookie
     else
-      curl_jar = F("-b %s", curl_cookie)
+      a[#a+1] = '-b'
+      a[#a+1] = cookie
     end
     if set.insecure then
-      curl_insecure = "-k"
+      a[#a+1] = '-k'
     end
-    set.curl = F("curl %s %s %s %s %s %s %s", curl_jar, headers, user_agent, defopts, defhdrs, curl_insecure or "", url)
-    local n, r = popen(set.curl)
-    if n == 0 then
-      return table.concat(r.output, "\n")
+    a[#a+1] = url
+    local n, r = exec.spawn('/usr/bin/curl', a)
+    if n then
+      return table.concat(r.stdout, "\n")
     else
-      return nil, F("error: popen: %s: %s", n, table.concat(r.output, "\n"))
+      return nil, F("error: exec:: %s: %s", n, table.concat(r.stderr, "\n"))
     end
   end})
 end
 local head = function()
-  local headers = [[-H 'Accept: */*']]
-  local opts = [[-o /dev/null -I -s -w '%%{http_code}']]
+  local a = clone(args)
+  a[#a+1] = '-H'
+  a[#a+1] = [['Accept: */*']]
+  a[#a+1] = '-o'
+  a[#a+1] = '/dev/null'
+  a[#a+1] = '-I'
+  a[#a+1] = '-s'
+  a[#a+1] = '-w'
+  a[#a+1] = [['%{http_code}']]
   local set = {}
   set.headers = {}
   set.insecure = false
   set.save = false
   return setmetatable(set, {__call = function(_, url)
-    local curl_cookie = set.cookie or "/tmp/"..util.random_string(16)
-    local curl_insecure, curl_jar, curl_data
+    local cookie = set.cookie or "/tmp/"..random_string(16)
     if next(set.headers) then
       for h, v in pairs(set.headers) do
-        headers = headers .. (F(" -H '%s: %s'", h, v))
+        a[#a+1] = '-H'
+        a[#a+1] = F([['%s: %s']], h, v)
       end
     end
     if set.save then
-      curl_jar = F("-b %s -c %s", curl_cookie, curl_cookie)
+      a[#a+1] = '-b'
+      a[#a+1] = cookie
+      a[#a+1] = '-c'
+      a[#a+1] = cookie
     else
-      curl_jar = F("-b %s", curl_cookie)
+      a[#a+1] = '-b'
+      a[#a+1] = cookie
     end
     if set.insecure then
-      curl_insecure = "-k"
+      a[#a+1] = '-k'
     end
-    set.curl = F([[curl %s %s %s %s %s %s %s %s]], opts, curl_jar, headers, user_agent, defopts, defhdrs, curl_insecure or "", url)
-    local n, r = popen(set.curl)
-    if n == 0 then
-      return table.concat(r.output, "\n")
+    a[#a+1] = url
+    local n, r = exec.spawn('/usr/bin/curl', a)
+    if n then
+      return table.concat(r.stdout, "\n")
     else
-      return nil, F("error: popen: %s: %s", n, table.concat(r.output, "\n"))
+      return nil, F("error: exec:: %s: %s", n, table.concat(r.stderr, "\n"))
     end
   end})
 end
