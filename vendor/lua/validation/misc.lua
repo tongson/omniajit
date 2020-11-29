da6d09b62a89259fc9c041488cf83065f2e7d933
Eduardo Tongson <propolice@gmail.com>
2020-11-29 15:51:20 +0800

    validation: Add hashing.

--- vendor/lua/validation/misc.lua
+++ vendor/lua/validation/misc.lua
@@ -3,11 +3,13 @@ local lh           = require "lhutil"
 local base64       = require "base64"
 local re           = require "re"
 local ammonia      = require "ammonia"
+local blake3       = require "blake3"
 local escapeuri    = lh.encodeURI
 local unescapeuri  = lh.decodeURI
 local htmlentities = ammonia.clean_text
 local base64enc    = base64.encode
 local base64dec    = base64.decode
+local hash         = blake3.hash
 local match        = re.match
 local validators   = validation.validators
 local factory      = getmetatable(validators)
@@ -45,17 +47,24 @@ function factory.regex(regex, options)
         return (match(value, regex, options)) ~= nil
     end
 end
+function factory.hash(bin)
+  return function(value)
+    local digest = bin and hash(value)
+    return true, digest
+  end
+end
 validators.escapeuri   = factory.escapeuri()
 validators.unescapeuri = factory.unescapeuri()
 validators.htmlentities = factory.htmlentities()
 validators.base64enc   = factory.base64enc()
 validators.base64dec   = factory.base64dec()
+validators.hash        = factory.hash()
 return {
     escapeuri   = validators.escapeuri,
     unescapeuri = validators.unescapeuri,
     htmlentities= validators.htmlentities,
     base64enc   = validators.base64enc,
     base64dec   = validators.base64dec,
-    md5         = validators.md5,
+    hash        = validators.hash,
     regex       = factory.regex
 }
