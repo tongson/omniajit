f2e2f369556a9f3587def99ecf25f31d4715fda9
Eduardo Tongson <propolice@gmail.com>
2020-12-08 19:07:48 +0800

    ammonia: Cleaner prototypes. Skip arg checks, seems superfluous.

--- vendor/lua/ammonia.lua
+++ vendor/lua/ammonia.lua
@@ -1,7 +1,7 @@
 local ffi = require "ffi"
 ffi.cdef [[
-const char *clean(const char *h);
-const char *clean_text(const char *h);
+const char *clean(const char *);
+const char *clean_text(const char *);
 ]]
 
 local p = package.ffipath
@@ -15,15 +15,9 @@ local M = ffi.load(p.."/libammonia_c.so")
 
 return {
     clean = function (s)
-        if not s then
-            return nil, "Missing argument."
-        end
         return ffi.string(M.clean(s))
     end,
     clean_text = function (s)
-        if not s then
-            return nil, "Missing argument."
-        end
         return ffi.string(M.clean_text(s))
     end,
 }
