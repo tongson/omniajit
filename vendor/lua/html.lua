de45493f5f13ba46aa4c1193511bec0715017f1c
Eduardo Tongson <propolice@gmail.com>
2020-12-24 15:34:28 +0800

    html: Load lhutil functions.

--- vendor/lua/html.lua
+++ vendor/lua/html.lua
@@ -1,23 +1,17 @@
-local ffi = require "ffi"
+local ffi = require 'ffi'
 ffi.cdef [[
 const char *clean(const char *);
 const char *clean_text(const char *);
 ]]
 
-local p = package.ffipath
-if p == nil then
-  return nil, "package.ffipath not set."
-elseif p == "/" then
-  p = "."
-end
-
+local p = package.__ffi or '.'
 local M = ffi.load(p.."/libammonia_c.so")
-
-return {
-    clean = function (s)
-        return ffi.string(M.clean(s))
-    end,
-    clean_text = function (s)
-        return ffi.string(M.clean_text(s))
-    end,
-}
+local T = require 'lhutil'
+-- These are HTML strings so just ignore 8-bit clean strings.
+T.clean = function (s)
+  return ffi.string(M.clean(s))
+end
+T.clean_text = function (s)
+  return ffi.string(M.clean_text(s))
+end
+return T
