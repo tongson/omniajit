99ae73f3d0dba0520b9fd7c1573baf94ce300308
Eduardo Tongson <propolice@gmail.com>
2020-12-24 13:06:50 +0800

    blake3: Deleted base64() function then base64 encode input to hash().

--- vendor/lua/blake3.lua
+++ vendor/lua/blake3.lua
@@ -1,23 +1,14 @@
-local ffi = require "ffi"
+local ffi = require 'ffi'
 ffi.cdef [[
-const char *base64(const char *);
 const char *hash(const char *);
 ]]
 
-local p = package.ffipath
-if p == nil then
-  return nil, "package.ffipath not set."
-elseif p == "/" then
-  p = "."
-end
-
-local M = ffi.load(p.."/libblake3_c.so")
+local B = require 'base64'
+local p = package.__ffi or '.'
+local M = ffi.load(p.."/libblake.so")
 
 return {
-    base64 = function (s)
-        return ffi.string(M.base64(s))
-    end,
     hash = function (s)
-        return ffi.string(M.hash(s))
+        return ffi.string(M.hash(B.encode(s)))
     end,
 }
