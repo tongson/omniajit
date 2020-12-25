4e473633dad6c5f6c7c1f3bbdf9923ff4c728786
Eduardo Tongson <propolice@gmail.com>
2020-12-25 18:42:42 +0800

    std: Pointless checks. string.format can handle single arguments.

--- vendor/lua/std.lua
+++ vendor/lua/std.lua
@@ -108,56 +108,31 @@ end
 
 local minfo = function(...)
   local str
-  if select("#", ...) == 1 then
-    str = (...)
-  else
-    str = F(...)
-  end
-  io.stdout:write(F("%s[%s] %s+ %sinfo  %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[34m", "\27[0m", str))
+  io.stdout:write(F("%s[%s] %s+ %sinfo  %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[34m", "\27[0m", F(str)))
   return io.stdout:flush()
 end
 
 local mok = function(...)
   local str
-  if select("#", ...) == 1 then
-    str = (...)
-  else
-    str = F(...)
-  end
-  io.stdout:write(F("%s[%s] %s* %sok    %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[32m", "\27[0m", str))
+  io.stdout:write(F("%s[%s] %s* %sok    %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[32m", "\27[0m", F(str)))
   return io.stdout:flush()
 end
 
 local mdebug = function(...)
   local str
-  if select("#", ...) == 1 then
-    str = (...)
-  else
-    str = F(...)
-  end
-  io.stdout:write(F("%s[%s] %s. %sdebug %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[33m", "\27[0m", str))
+  io.stdout:write(F("%s[%s] %s. %sdebug %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[33m", "\27[0m", F(str)))
   return io.stdout:flush()
 end
 
 local mfatal = function(...)
   local str
-  if select("#", ...) == 1 then
-    str = (...)
-  else
-    str = F(...)
-  end
-  io.stderr:write(F("%s[%s] %s! %sfatal %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[31m", "\27[0m", str))
+  io.stderr:write(F("%s[%s] %s! %sfatal %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[31m", "\27[0m", F(str)))
   return io.stderr:flush()
 end
 
 local mwarn = function(...)
   local str
-  if select("#", ...) == 1 then
-    str = (...)
-  else
-    str = F(...)
-  end
-  io.stderr:write(F("%s[%s] %s? %swarn  %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[31m", "\27[0m", str))
+  io.stderr:write(F("%s[%s] %s? %swarn  %s%s\n",  "\27[35m", os.date("%H:%M:%S"), "\27[36m", "\27[31m", "\27[0m", F(str)))
   return io.stderr:flush()
 end
 
