fa3442b912c697f43a0fa0cdb1ff186058ad43d5
Eduardo Tongson <propolice@gmail.com>
2020-12-28 18:02:58 +0800

    std: Does not work as expected.

--- vendor/lua/std.lua
+++ vendor/lua/std.lua
@@ -324,8 +324,8 @@ end
 
 local truthy = function(s)
   local _
-  _, s = pcall(string.lower, tostring(s))
-  if s == "yes" or s == "true" or s == "on" or s == "1" or s == "1LL" then
+  _, s = pcall(string.lower, s)
+  if s == "yes" or s == "true" or s == "on" or s == "1" then
     return true
   else
     return false
@@ -334,8 +334,8 @@ end
 
 local falsy = function(s)
   local _
-  _, s = pcall(string.lower, tostring(s))
-  if s == "no" or s == "false" or s == "off" or s == "0" or s == "1LL" then
+  _, s = pcall(string.lower, s)
+  if s == "no" or s == "false" or s == "off" or s == "0" then
     return true
   else
     return false
