97c717566667d1a55b85234e5e892f1a0551fa0a
Eduardo Tongson <propolice@gmail.com>
2020-12-30 18:07:49 +0800

    web: A missing arg.path unlikely has the ffi key also set.

--- vendor/lua/web.lua
+++ vendor/lua/web.lua
@@ -7,8 +7,7 @@ const char *clean(const char *);
 const char *clean_text(const char *);
 ]]
 
-local p = arg.path.ffi or '.'
-local M = ffi.load(p..'/libhtml.so')
+local M = ffi.load(arg.path.ffi..'/libhtml.so')
 local T = require 'lhutil'
 -- These are HTML strings so just ignore 8-bit clean strings.
 T.clean = function (s)
