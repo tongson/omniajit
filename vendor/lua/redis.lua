749fd33baa59fb7059a5b9b0d8aced299c83b938
Eduardo Tongson <propolice@gmail.com>
2021-03-28 01:02:52 +0800

    redis: Sync with ljredis. Use exit codes and add HSET feature.

--- vendor/lua/redis.lua
+++ vendor/lua/redis.lua
@@ -1,104 +1,117 @@
 local ffi = require 'ffi'
 ffi.cdef [[
-const char *del(const char *);
-const char *incr(const char *);
-const char *json_get(const char *);
-const char *get(const char *);
-const char *json_set(const char *);
-const char *set(const char *);
+int del(const char *, const char *);
+int incr(const char *, const char *);
+int json_get(const char *, const char *, unsigned char *);
+int get(const char *, const char *, unsigned char *);
+int json_set(const char *, const char *);
+int set(const char *, const char *);
+int hset(const char *, const char *);
 ]]
-local M = ffi.load(arg.path.ffi..'/librediz.so')
-local C = string.char
+local M = ffi.load(arg.path.ffi..'/libljredis.so.0.1.0')
+local F = string.format
 local J = require 'json'
 local B = require 'base64'
 
+local LOCALHOST = '127.0.0.1'
+local ECONN = -13
+local ECLIENT = -43
+local EINVALID = -7
+local EQUERY = -4
+local OK = 0
+local MAX = 536870912
+
+local E = function(n, r)
+  local rt = {
+    [ECONN]    = F('redis.%s: Unable to establish connection with Redis.', n),
+    [ECLIENT]  = F('redis.%s: Failure preparing Redis client.', n),
+    [EQUERY]   = F('redis.%s: Problem with the Redis query.', n),
+    [EINVALID] = F('redis.%s: Invalid JSON.', n),
+  }
+  return rt[r] or F('redis.%s: Invalid return value.', n)
+end
+
 return {
-  del = function(k)
-    local r = ffi.string(M.del(k))
-    if     r == C(6) then
+  del = function(k, h)
+    h = h or LOCALHOST
+    local r = M.del(h, k)
+    if r == OK then
       return true
-    elseif r == C(21) then
-      return nil, "redis.del: Error in query."
-    elseif r == C(20) then
-      return nil, "redis.del: Unable to connect to redis."
-    elseif r == C(18) then
-      return nil, "redis.del: Error preparing client."
+    else
+      return nil, E('del', r)
     end
   end,
-  incr = function(k)
-    local r = ffi.string(M.incr(k))
-    if     r == C(6) then
+  incr = function(k, h)
+    h = h or LOCALHOST
+    local r = M.incr(h, k)
+    if r == OK then
       return true
-    elseif r == C(21) then
-      return nil, "redis.incr: Error in query."
-    elseif r == C(20) then
-      return nil, "redis.incr: Unable to connect to redis."
-    elseif r == C(18) then
-      return nil, "redis.incr: Error preparing client."
-    end
-  end,
-  json_get = function(k)
-    local r = ffi.string(M.json_get(J.encode(k)))
-    if     r == C(21) then
-      return nil, "redis.json_get: Error in query."
-    elseif r == C(20) then
-      return nil, "redis.json_get: Unable to connect to redis."
-    elseif r == C(18) then
-      return nil, "redis.json_get: Error preparing client."
-    elseif r == "" then
-      return nil, "redis.json_get: Empty."
     else
-      return J.decode(r)
+      return nil, E('incr', r)
     end
   end,
-  get = function(k)
-    local r = ffi.string(M.get(k))
-    if     r == C(21) then
-      return nil, "redis.get: Error in query."
-    elseif r == C(20) then
-      return nil, "redis.get: Unable to connect to redis."
-    elseif r == C(18) then
-      return nil, "redis.get: Error preparing client."
-    elseif r == "" then
-      return nil, "redis.get: Empty."
+  json_get = function(k, h)
+    h = h or LOCALHOST
+    local b = ffi.new('unsigned char[?]', MAX)
+    local r = M.json_get(h, J.encode(k), b)
+    if r > 0 then
+      return J.decode(ffi.string(b, r))
     else
-      return r
+      return nil, E('json_get', r)
     end
   end,
-  json_set = function(t)
+  get = function(k, h)
+    h = h or LOCALHOST
+    local b = ffi.new('unsigned char[?]', MAX)
+    local r = M.get(h, k, b)
+    if r > 0 then
+      return ffi.string(b, r)
+    else
+      return nil, E('get', r)
+    end
+  end,
+  json_set = function(t, h)
+    h = h or LOCALHOST
     if not t.nx then
       t.nx = "false"
     else
       t.nx = "true"
     end
     t.data = B.encode(J.encode(t.data))
-    local r = ffi.string(M.json_set(J.encode(t)))
-    if     r == C(6) then
+    local r = M.json_set(h, J.encode(t))
+    if r == OK then
       return true
-    elseif r == C(21) then
-      return nil, "redis.json_set: Error in query."
-    elseif r == C(20) then
-      return nil, "redis.json_set: Unable to connect to redis."
-    elseif r == C(18) then
-      return nil, "redis.json_set: Error preparing client."
+    else
+      return nil, E('json_set', r)
     end
   end,
-
-  set = function(t)
+  set = function(t, h)
+    h = h or LOCALHOST
     if not t.expire then
       t.expire = "0"
     else
       t.expire = tostring(t.expire)
     end
-    local r = ffi.string(M.set(J.encode(t)))
-    if     r == C(6) then
+    local r = M.set(h, J.encode(t))
+    if r == OK then
       return true
-    elseif r == C(21) then
-      return nil, "redis.set: Error in query."
-    elseif r == C(20) then
-      return nil, "redis.set: Unable to connect to redis."
-    elseif r == C(18) then
-      return nil, "redis.set: Error preparing client."
+    else
+      return nil, E('set', r)
     end
   end,
+  hset = function(t, h)
+    h = h or LOCALHOST
+    if not t.expire then
+      t.expire = "0"
+    else
+      t.expire = tostring(t.expire)
+    end
+    local r = M.hset(h, J.encode(t))
+    if r == OK then
+      return true
+    else
+      return nil, E('hset', r)
+    end
+  end,
+
 }
