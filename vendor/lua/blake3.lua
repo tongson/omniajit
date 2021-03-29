f173a7e71fbbce8ef169a8f3597d27ef5c0f85af
Eduardo Tongson <propolice@gmail.com>
2021-03-29 12:03:10 +0800

    blake3: Sync with FFI side.

--- vendor/lua/blake3.lua
+++ vendor/lua/blake3.lua
@@ -1,15 +1,15 @@
 local ffi = require 'ffi'
 ffi.cdef [[
-int *hash(const char *, unsigned char*, size_t);
+int *hash(unsigned char*, size_t, unsigned char*);
 ]]
-
-local B = require 'base64'
 local M = ffi.load(arg.path.ffi.."/libblake.so.a3e0958")
-
 return {
   hash = function (s)
     local b = ffi.new("unsigned char[?]", 64)
-    M.hash(B.encode(s), b, 64)
+    local l = #s
+    local p = ffi.new("unsigned char[?]", l)
+    ffi.copy(p, s, l)
+    M.hash(p, l, b)
     return ffi.string(b, 64)
   end,
 }
